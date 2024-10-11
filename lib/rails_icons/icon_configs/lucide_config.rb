fa9f0165a0d18e3dec5ac025108bb084abc3d8ca
Jordan Calhoun <jordancalhoun@gmail.com>
2024-10-10 20:12:34 -0400

    fix grammer in lucide_config

--- lib/rails_icons/icon_configs/lucide_config.rb
+++ lib/rails_icons/icon_configs/lucide_config.rb
@@ -5,19 +5,19 @@ module RailsIcons
   class LucideConfig
     def config
       options = ActiveSupport::OrderedOptions.new
-      setup_lucide_outlined_config(options)
+      setup_lucide_outline_config(options)
 
       options
     end
 
     private
 
-    def setup_lucide_outlined_config(options)
+    def setup_lucide_outline_config(options)
       options.outline = ActiveSupport::OrderedOptions.new
       options.outline.default = default_outlined_options
     end
 
-    def default_outlined_options
+    def default_outline_options
       options = ActiveSupport::OrderedOptions.new
       options.stroke_width = "1.5"
       options.css = "w-6 h-6"
