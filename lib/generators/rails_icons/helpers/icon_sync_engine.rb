e2e790ee9ca8a09a6dc06fbafdce0c8cf209f85e
Jordan Calhoun <jordancalhoun@gmail.com>
2024-10-10 20:12:07 -0400

    break methods apart, rename sync engine

--- lib/generators/rails_icons/helpers/icon_sync_engine.rb
+++ lib/generators/rails_icons/helpers/icon_sync_engine.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-require 'fileutils'
+require "fileutils"
 
 module RailsIcons
   # Pulls the latest set of icon svgs from the given repository
   class IconSyncEngine < Rails::Generators::Base
     def initialize(temp_dir, repository)
       super
-      @temp_dir = File.join(temp_dir, repository[:name])
+      @temp_dir = File.join(temp_dir, repository[:name], repository[:dir_wrapper])
       @repository = repository
     end
 
     def sync
       clone_repo
-      move_icons_folder
+      filter_icons_from_repo
       remove_non_svg_files
-    rescue StandardError => e
+    rescue => e
       say "Failed to sync icons: #{e.message}", :red
       clean_up
       raise
@@ -26,7 +26,7 @@ module RailsIcons
     # If sync fails offer to clean up any files that were downloaded
     def clean_up
       if yes?("Do you want to remove the temp files? ('#{@temp_dir}')")
-        say 'Cleaning up...'
+        say "Cleaning up..."
         FileUtils.rm_rf(@temp_dir)
       else
         say("Leaving files at: '#{@temp_dir}'")
@@ -35,33 +35,44 @@ module RailsIcons
 
     # Clone the given repo into temp_dir/<Icon set name>
     def clone_repo
-      raise 'Failed to clone Icon repository' unless system("git clone '#{@repository[:repo_url]}' '#{@temp_dir}'")
+      raise "Failed to clone Icon repository" unless system("git clone '#{@repository[:repo_url]}' '#{@temp_dir}'")
 
-      say 'Icon set cloned successfully', :green
+      say "Icon set cloned successfully", :green
     end
 
-    # Move the 'icons' folder out of the repo and
-    # delete the all other repository files
-    def move_icons_folder
-      icons_dir = File.join(@temp_dir, @repository[:icon_dir])
-
+    def filter_icons_from_repo
       raise "Failed to find the icons directory: '#{icons_dir}'" unless Dir.exist?(icons_dir)
 
       # Store the list of current items in the root directory for reference
       # when deleting files later
-      original_temp_dir_items = Dir.entries(@temp_dir) - ['.', '..']
+      original_temp_dir_items = Dir.entries(@temp_dir) - [".", ".."]
 
+      move_icons
+
+      # Remove everything in the root directory (@temp_dir) that was originally there
+      remove_files_and_folders(original_temp_dir_items)
+
+      say "Icons filtered from repository successfully.", :green
+    end
+
+    def icons_dir
+      File.join(@temp_dir, @repository[:icon_dir])
+    end
+
+    def move_icons
       # Move all contents of the icons directory to the parent directory (temp_dir)
       Dir.foreach(icons_dir) do |item|
-        next if ['.', '..'].include?(item) # Skip special directories
+        next if [".", ".."].include?(item) # Skip special directories
 
         item_path = File.join(icons_dir, item)
         # Move the item to the temp_dir (root level)
         FileUtils.mv(item_path, @temp_dir)
       end
+    end
 
+    def remove_files_and_folders(dir_items)
       # Remove everything in the root directory (@temp_dir) that was originally there
-      original_temp_dir_items.each do |item|
+      dir_items.each do |item|
         item_path = File.join(@temp_dir, item)
         if File.directory?(item_path)
           FileUtils.rm_rf(item_path) # Remove directory and its contents
@@ -69,21 +80,18 @@ module RailsIcons
           FileUtils.rm(item_path) # Remove file
         end
       end
-
-      say 'Icons moved and cleaned up successfully.', :green
     end
 
     # Some repositories have .json files inside their icon directories
     # Remove everything but the .svg files
     def remove_non_svg_files
       Dir.glob("#{@temp_dir}/**/*") do |file|
-        if File.file?(file) && File.extname(file) != '.svg'
+        if File.file?(file) && File.extname(file) != ".svg"
           FileUtils.rm(file) # Remove the file
           puts "Deleted: #{file}"
         end
       end
-      say 'Non-SVG files removed successfully.', :green
+      say "Non-SVG files removed successfully.", :green
     end
   end
 end
-
--- lib/generators/rails_icons/sync_generator.rb
+++ lib/generators/rails_icons/sync_generator.rb
@@ -1,108 +1,86 @@
 # frozen_string_literal: true
 
-require 'fileutils'
+require "fileutils"
 
-require_relative 'icon_repos/tabler_sync_engine'
+require_relative "helpers/icon_sync_engine"
 module RailsIcons
   class SyncGenerator < Rails::Generators::Base
-    ICON_VAULT_REPO_URL = 'https://github.com/Rails-Designer/rails_icons_vault.git'
-
     REPOSITORIES = {
       heroicons: {
-        name: 'heroicons',
-        repo_url: 'https://github.com/tailwindlabs/heroicons.git',
-        icon_dir: 'src/24'
+        name: "heroicons",
+        repo_url: "https://github.com/tailwindlabs/heroicons.git",
+        icon_dir: "src/24",
+        dir_wrapper: ""
       },
       tabler: {
-        name: 'tabler',
-        repo_url: 'https://github.com/tabler/tabler-icons.git',
-        icon_dir: 'icons'
+        name: "tabler",
+        repo_url: "https://github.com/tabler/tabler-icons.git",
+        icon_dir: "icons",
+        dir_wrapper: ""
       },
       lucide: {
-        name: 'lucide',
-        repo_url: 'https://github.com/lucide-icons/lucide.git',
-        icon_dir: 'icons'
+        name: "lucide",
+        repo_url: "https://github.com/lucide-icons/lucide.git",
+        icon_dir: "icons",
+        dir_wrapper: "/outline"
       }
-    }
+    }.freeze
 
-    argument :libraries, type: :array, default: [], banner: 'heroicons lucide tabler'
+    argument :libraries, type: :array, default: [], banner: "heroicons lucide tabler"
 
     class_option :destination, type: :string, default: nil,
-                               desc: 'Custom destination folder for icons (default: `app/assets/svg/icons/`)'
+      desc: "Custom destination folder for icons (default: `app/assets/svg/icons/`)"
 
-    desc 'Sync a specified icon set(s) from the Rails Icons Vault (https://github.com/Rails-Designer/rails_icons_vault)'
-    source_root File.expand_path('templates', __dir__)
+    desc "Sync a specified icon set(s) from the Rails Icons Vault (https://github.com/Rails-Designer/rails_icons_vault)"
+    source_root File.expand_path("templates", __dir__)
 
     def sync_icons
-      icons_dir = options[:destination] || Rails.root.join('app/assets/svg/icons')
-      tmp_dir = Rails.root.join('tmp/icons')
+      icons_dir = options[:destination] || default_icons_dir
+      tmp_dir = tmp_icons_dir
 
-      # Prepare temp directory for sync
-      FileUtils.rm_rf(tmp_dir) if Dir.exist?(tmp_dir)
+      clean_tmp_dir(tmp_dir)
 
-      libraries.each do |set|
-        repo = REPOSITORIES[set.to_sym]
-        IconSyncEngine.new(tmp_dir, repo).sync
+      libraries.each { |set| sync_icon_set(set, tmp_dir, icons_dir) }
 
-        set_path = File.join(tmp_dir, set)
-
-        if Dir.exist?(set_path)
-          destination_path = File.join(icons_dir, set)
-
-          FileUtils.mkdir_p(destination_path)
-          FileUtils.cp_r(Dir.glob("#{set_path}/*"), destination_path)
-
-          say "Synced `#{set}` icons for set", :green
-        else
-          say "Icon set `#{set}` not found", :red
-
-          exit 1
-        end
-      end
-
-      # Clean up temp directory
-      FileUtils.rm_rf(tmp_dir)
-
-      say 'Icons synced successfully', :green
+      clean_tmp_dir(tmp_dir)
     end
 
     private
 
-    def oldsync
-      icons_dir = options[:destination] || Rails.root.join('app/assets/svg/icons')
-      tmp_dir = Rails.root.join('tmp/icons')
+    def default_icons_dir
+      Rails.root.join("app/assets/svg/icons")
+    end
 
+    def tmp_icons_dir
+      Rails.root.join("tmp/icons")
+    end
+
+    def clean_tmp_dir(tmp_dir)
       FileUtils.rm_rf(tmp_dir) if Dir.exist?(tmp_dir)
-      FileUtils.mkdir_p(tmp_dir)
+    end
 
-      if system("git clone '#{ICON_VAULT_REPO_URL}' '#{tmp_dir}'")
-        say 'Repository cloned successfully.', :green
+    def sync_icon_set(set, tmp_dir, icons_dir)
+      repo = REPOSITORIES[set.to_sym]
+      IconSyncEngine.new(tmp_dir, repo).sync
+      icon_set_path = File.join(tmp_dir, set)
+
+      if Dir.exist?(icon_set_path)
+        copy_icon_set(set, icon_set_path, icons_dir)
       else
-        say 'Failed to clone repository.', :red
-
-        exit 1
+        log_icon_set_not_found(set)
       end
+    end
 
-      libraries.each do |set|
-        set_path = File.join(tmp_dir, 'icons', set)
+    def copy_icon_set(set, set_path, icons_dir)
+      destination_path = File.join(icons_dir, set)
+      FileUtils.mkdir_p(destination_path)
+      FileUtils.cp_r(Dir.glob("#{set_path}/*"), destination_path)
+      say "Synced `#{set}` icons successfully.", :green
+    end
 
-        if Dir.exist?(set_path)
-          destination_path = File.join(icons_dir, set)
-
-          FileUtils.mkdir_p(destination_path)
-          FileUtils.cp_r(Dir.glob("#{set_path}/*"), destination_path)
-
-          say "Synced `#{set}` icons for set", :green
-        else
-          say "Icon set `#{set}` not found", :red
-
-          exit 1
-        end
-      end
-
-      FileUtils.rm_rf(tmp_dir)
-
-      say 'Icons synced successfully', :green
+    def log_icon_set_not_found(set)
+      say "Icon set `#{set}` not found.", :red
+      exit 1
     end
   end
 end
