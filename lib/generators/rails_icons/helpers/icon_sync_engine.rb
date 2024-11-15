86f7383209a97e2a3a755647535bcf6ab51919c6
Jordan Calhoun <jordancalhoun@gmail.com>
2024-11-15 11:38:45 -0500

    feat(sync): Update sync engine for variant support

--- lib/generators/rails_icons/helpers/icon_sync_engine.rb
+++ lib/generators/rails_icons/helpers/icon_sync_engine.rb
@@ -7,7 +7,7 @@ module RailsIcons
   class IconSyncEngine < Rails::Generators::Base
     def initialize(temp_dir, repository)
       super
-      @temp_dir = File.join(temp_dir, repository[:name], repository[:parent_dir])
+      @temp_dir = File.join(temp_dir, repository[:name])
       @repository = repository
     end
 
@@ -36,31 +36,40 @@ module RailsIcons
     # Clone the given repo into temp_dir/<Icon set name>
     def clone_repo
       raise "Failed to clone Icon repository" unless system("git clone '#{@repository[:repo_url]}' '#{@temp_dir}'")
-      say "Icon set cloned successfully", :green
+      say "#{@repository[:name]} repository cloned successfully.", :green
     end
 
     def filter_icons_from_repo
-      raise "Failed to find the icons directory: '#{icons_dir}'" unless Dir.exist?(icons_dir)
       # Store the list of current items in the root directory for reference
       # when deleting files later
       original_temp_dir_items = Dir.entries(@temp_dir) - [ ".", ".." ]
-      move_icons
+
+      @repository[:variants].each do |folder_name, repo_path|
+        source_dir = File.join(@temp_dir, repo_path)
+        dest_dir = File.join(@temp_dir, folder_name.to_s)
+
+        raise "Failed to find the icons directory: '#{source_dir}'" unless Dir.exist?(source_dir)
+
+        move_icons(source_dir, dest_dir)
+      end
+
       # Remove everything in the root directory (@temp_dir) that was originally there
+      # TODO: Edge case: The variant for the icons name is the same as a folder that already exists, this will delete the variant on clean up.  Update method check against the variant directories and ignore them on removal.
       remove_files_and_folders(original_temp_dir_items)
-      say "Icons filtered from repository successfully.", :green
+      say "Icon variants filtered successfully.", :green
     end
 
-    def icons_dir
-      File.join(@temp_dir, @repository[:source_dir])
-    end
-
-    def move_icons
-      # Move all contents of the icons directory to the parent directory (temp_dir)
-      Dir.foreach(icons_dir) do |item|
+    def move_icons(source_dir, dest_dir)
+      # Move all contents of the variant source directory to the parent directory/variant (temp_dir)
+      Dir.foreach(source_dir) do |item|
         next if [ ".", ".." ].include?(item) # Skip special directories
-        item_path = File.join(icons_dir, item)
-        # Move the item to the temp_dir (root level)
-        FileUtils.mv(item_path, @temp_dir)
+        item_path = File.join(source_dir, item)
+
+        # Ensure the variant directory exists, create it if needed
+        FileUtils.mkdir_p(dest_dir)
+
+        # Move the item to the variants directory at the root level
+        FileUtils.mv(item_path, dest_dir)
       end
     end
 
@@ -82,9 +91,9 @@ module RailsIcons
       Dir.glob("#{@temp_dir}/**/*") do |file|
         if File.file?(file) && File.extname(file) != ".svg"
           FileUtils.rm(file) # Remove the file
-          puts "Deleted: #{file}"
         end
       end
+
       say "Non-SVG files removed successfully.", :green
     end
   end
